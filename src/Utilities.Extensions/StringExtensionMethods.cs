db382904d2e2d7865da414fad4fa4181dfd8eb3f
Ryan Buckley <9486206+rabuckley@users.noreply.github.com>
2022-03-04 20:12:07 +0000

    Update

---
+++ src/Utilities.Extensions/StringExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿namespace Utilities.Commands;
+
+static class StringExtensionMethods
+{
+    public static string RemoveAll(this string target, string remove, string replace)
+    {
+        if (!target.Contains(remove)) return target;
+
+        while (target.Contains(remove))
+            target = target.Replace(remove, replace);
+
+        return target;
+    }
+}
