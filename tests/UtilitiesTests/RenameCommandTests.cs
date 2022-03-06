ea4514e8409e4fe32d45b1d81e005b1bca16594b
Ryan Buckley <9486206+rabuckley@users.noreply.github.com>
2022-03-06 13:47:42 +0000

    Remove File System Tests

--- tests/UtilitiesTests/RenameCommandTests.cs
+++ tests/UtilitiesTests/RenameCommandTests.cs
@@ -38,24 +38,4 @@ public class RenameCommandTests
         Assert.False(actual.UnmatchedTokens.Any());
         Assert.Equal(1, actual.Tokens.Count);
     }
-
-    [Fact]
-    public void RenamesCorrectly()
-    {
-        // Arrange
-        var renameCommand = new RenameCommand();
-        EnsureFilesExists("README.md");
-
-        // Act
-        renameCommand.Invoke("README.md");
-
-        // Assert
-        Assert.True(File.Exists("readme.md"));
-    }
-
-    private static void EnsureFilesExists(string fileName)
-    {
-        if (!File.Exists(fileName)) File.Create(fileName);
-        if (!File.Exists(fileName)) throw new Exception("Couldn't find file");
-    }
 }
